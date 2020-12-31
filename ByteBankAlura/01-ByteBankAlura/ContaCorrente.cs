4fd6fb5c1edafba0baaca6e9e2901b46ba6b0508
italofalves <italofalves@hotmail.com>
2020-12-30 22:38:12 -0300

    Alura C# ByteBank

--- ByteBankAlura/01-ByteBankAlura/ContaCorrente.cs
+++ ByteBankAlura/01-ByteBankAlura/ContaCorrente.cs
@@ -10,7 +10,29 @@ namespace _01_ByteBankAlura // Sempre colocar o namespace nas classes se não ti
         public Cliente titular;
         public int agencia;
         public int numero;
-        public double saldo;
+        private double saldo; // colocando privado para nao termos acesso de fora, podendo ser acessado por metodos.
+
+
+        // Colocamos void no metodo abaixo, por que não precisa ter um retorno para o usuario. 
+        public void DefinirSaldo (double saldo)
+        {
+            if ( saldo < 0)
+            {
+                return; // Quando temos um return num metodo void, estamos parando o metodo;
+            }
+            else
+            {
+                this.saldo = saldo; // Com o this estamos acessando o saldo do "private"
+            }
+
+        }
+
+
+        // isso é um metodo, e todo metodo tem que colocar parenteses depois.
+        public double ObterSaldo()
+        {
+            return saldo;
+        }
 
         public bool Sacar(double valor) // Double dentro do parenteses, pois o valor do saldo é double // bool para saber se o saque foi feito com sucesso ou não.
         {
--- ByteBankAlura/01-ByteBankAlura/Program.cs
+++ ByteBankAlura/01-ByteBankAlura/Program.cs
@@ -18,13 +18,15 @@ namespace _01_ByteBankAlura
             contaCorrenteItalo.titular = ClienteItalo;
             contaCorrenteItalo.agencia = 893;
             contaCorrenteItalo.numero = 89342;
-            contaCorrenteItalo.saldo = 1000;
+            contaCorrenteItalo.DefinirSaldo(200); // Estamos atribuindo um valor no campo de saldo privado atraves de um metodo. 
+            //contaCorrenteItalo.saldo = 1000;  //mudamos o codigo, e colocanmos um metodo para mostrar o saldo. 
 
-            Console.WriteLine("Titular: " + contaCorrenteItalo.titular.nome)); // se não colocar o ponto "." e o caminho, estarei acessando apenas o objeto.
+            Console.WriteLine("Titular: " + contaCorrenteItalo.titular.nome); // se não colocar o ponto "." e o caminho, estarei acessando apenas o objeto.
             Console.WriteLine("Titular: " + contaCorrenteItalo.titular.ToString()); //Com esse metodo estarei acessando todas as informações na classe do cliente. 
             Console.WriteLine("Agência: " + contaCorrenteItalo.agencia);
             Console.WriteLine("Numero da conta: " + contaCorrenteItalo.numero);
-            Console.WriteLine("Saldo disponivel: " + contaCorrenteItalo.saldo);
+            //Console.WriteLine("Saldo disponivel: " + contaCorrenteItalo.saldo); Mudamos o jeito para visualizar o saldo, podendo ser visto qual é o saldo atraves de um metodo.
+            Console.WriteLine("Saldo Disponivel R$ " + contaCorrenteItalo.ObterSaldo()); // colocamos esse metodo para mostrar o saldo. Lembrando que o saldo se tornou "private"
 
             //contaCorrenteItalo.Sacar(valor): Esse comando seria para sacar dinheiro. colocando o valor desejado dentro do "valor".
             //contaCorrenteItalo.Depositar(valor): Esse comando é para depositar, colocando o valor desejado dentro do "valor"; não tem como guardar dentro de uma variavel porque não retorna nada.
